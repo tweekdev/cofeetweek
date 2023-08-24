9eb1505dcff3e00bbd475f3b2df183612e248812
tweekdev <tja.theau54@gmail.com>
2023-08-24 15:53:24 +0200

    Initial Commit

---
+++ tweekCoffee/tweekCoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tweekCoffeeApp.swift
+//  tweekCoffee
+//
+//  Created by Théau Antoine on 24/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct tweekCoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
